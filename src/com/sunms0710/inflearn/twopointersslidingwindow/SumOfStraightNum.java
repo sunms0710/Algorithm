fe1e9374141e9effe10276c5edb3c1506066065a
sunms0710 <sunms0710@naver.com>
2022-07-02 19:17:08 +0900

    .

---
+++ src/com/sunms0710/inflearn/twopointersslidingwindow/SumOfStraightNum.java
@@ -0,0 +1,45 @@
+package com.sunms0710.inflearn.twopointersslidingwindow;
+
+import java.util.Scanner;
+
+//연속된 자연수의 합
+public class SumOfStraightNum {
+
+    //two pointers algorithm 이용
+    public static int solution(int n){
+        int answer = 0, sum=0 , lt = 0;
+        int m = n/2+1;
+        int[] arr = new int[m];
+        for(int i = 0; i < m; i++){
+            arr[i] = i + 1;
+        }
+        for(int rt = 0; rt < m; rt++){
+            sum += arr[rt];
+            if(sum == n) answer++;
+            while (sum >= n){
+                sum -= arr[lt++];
+                if(sum == n) answer++;
+            }
+        }
+        return answer;
+    }
+
+    //수학적 방법 이용
+    public static int solution2(int n){
+       int answer = 0, cnt = 1;
+       n--;
+       while(n>0){
+           cnt++;
+           n = n - cnt;
+           if(n % cnt == 0) answer++;
+       }
+       return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+        System.out.println(solution2(n));
+    }
+}
