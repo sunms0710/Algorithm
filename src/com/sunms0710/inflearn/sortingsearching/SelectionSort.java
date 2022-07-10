3849cf35ae8bc99b134286ad2b3fef563dc110a7
sunms0710 <sunms0710@naver.com>
2022-07-10 18:03:47 +0900

    .

---
+++ src/com/sunms0710/inflearn/sortingsearching/SelectionSort.java
@@ -0,0 +1,40 @@
+package com.sunms0710.inflearn.sortingsearching;
+
+import java.util.Scanner;
+
+/**
+ * 선택 정렬
+ * input
+ * 6
+ * 13 5 11 7 23 15
+ * output
+ * 5 7 11 13 15 23
+ */
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static int[] solution(int n, int[] arr) {
+        for(int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] < arr[idx]){
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+}
