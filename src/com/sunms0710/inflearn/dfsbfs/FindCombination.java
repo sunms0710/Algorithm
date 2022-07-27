d0bfe4d041e6827b884067376374c1a3a23ffef6
sunms0710 <sunms0710@naver.com>
2022-07-27 15:52:59 +0900

    .

---
+++ src/com/sunms0710/inflearn/dfsbfs/FindCombination.java
@@ -0,0 +1,30 @@
+package com.sunms0710.inflearn.dfsbfs;
+
+import java.util.Scanner;
+
+/**
+ * 조합 구하기
+ */
+public class FindCombination {
+    static int[] combi;
+    static int n, m;
+    public static void DFS(int L, int s){
+        if(L==m){
+            for(int x : combi) System.out.print(x + " ");
+            System.out.println();
+        }else {
+            for(int i = s; i <= n; i++){
+                combi[L] = i;
+                DFS(L+1, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        combi = new int[m];
+        DFS(0,1);
+    }
+}
---
+++ src/com/sunms0710/inflearn/dfsbfs/GuessSequence.java
@@ -0,0 +1,52 @@
+package com.sunms0710.inflearn.dfsbfs;
+
+import java.util.Scanner;
+
+/**
+ * 수열 추측하기
+ *
+ */
+public class GuessSequence {
+    static int[] b, p, ch;
+    static int n, f;
+    static boolean flag = false;
+    static int[][] dy = new int[35][35];
+
+    public static int combi(int n, int r){
+        if(dy[n][r]>0) return dy[n][r];
+        if(n==r || r==0) return 1;
+        else return dy[n][r] = combi(n-1,r-1) + combi(n-1,r);
+    }
+
+    public static void DFS(int L, int sum){
+        if(flag) return;
+        if(L==n){
+            if(sum==f){
+                for(int x : p) System.out.print(x + " ");
+                flag = true;
+            }
+        }else{
+            for(int i = 1; i <=n; i++){
+                if(ch[i] == 0){
+                    ch[i] = 1;
+                    p[L] = i;
+                    DFS(L+1, sum+(p[L] * b[L]));
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        f = sc.nextInt();
+        b = new int[n];
+        p = new int[n];
+        ch = new int[n+1];
+        for(int i = 0; i < n; i++){
+            b[i] = combi(n-1,i);
+        }
+        DFS(0,0);
+    }
+}
